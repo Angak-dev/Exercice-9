5c574b7d69a8096a8bd6ef7d2cbd494169f3c4f8
Bourassa <bourassa30@gmail.com>
2020-03-06 14:18:47 -0500

    3e commit exercice 9

--- Exercice9-master/functions.php
+++ Exercice9-master/functions.php
@@ -8,7 +8,7 @@
 - En ordre ascendant
  */
 function extraire_evenements( $query ) {
-    if ($query->is_category('evenements'))
+    if ($query->is_category('evenement'))
     {
        $query->set( 'posts_per_page', -1 );
        $query->set( 'orderby', 'date' );
