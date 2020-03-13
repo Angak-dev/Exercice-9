7cb844d391ab5b914eedbbe65b762628620ab6f3
Bourassa <bourassa30@gmail.com>
2020-03-13 16:02:10 -0400

    Commit tentative dajout du menu burger

--- Exercice9-master/header.php
+++ Exercice9-master/header.php
@@ -44,9 +44,10 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
+		<input type="checkbox" id="chk_menu"> 	 	
+		<label id="bouton_menu" for="chk_menu">&#9776;</label>
 		<nav id="site-navigation" class="main-navigation">
-			<input type="checkbox" id="chk_menu"> 	 	
-			<label id="bouton_menu" for="chk_menu">&#9776;</label>
+			
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
